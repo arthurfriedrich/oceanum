385f8f477d5b5e7e10d51d944f9f867facb93b74
Arthur Friedrich <85907112+arthurfriedrich@users.noreply.github.com>
2025-07-06 18:26:35 +0100

    Update 1_Oceanum.py

--- oceanum_website/1_Oceanum.py
+++ oceanum_website/1_Oceanum.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import time
 
-st.image("Assinatura.horizontal.principal-10.png", caption=None, width=None, use_column_width=None)
+st.image("logo.png", caption=None, width=None, use_column_width=None)
 st.markdown("# Welcome to Oceanum")
 st.text("We are working for a new and better webpage... ")
 
